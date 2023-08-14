4e92ba67a81ba1fd0117c74f36b24ba28923973e
rossparsons1 <kataisk2002@gmail.com>
2023-08-14 20:59:11 +0500

    New feature

---
+++ src/script.js
@@ -0,0 +1,26 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
+
+function previousSlide() {
+    showSlides(slideIndex -= 1);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("item");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    /* Проходим по каждому слайду в цикле for: */
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+    /* Делаем элемент блочным: */
+    slides[slideIndex - 1].style.display = "block";
+}
