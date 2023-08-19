ea8e99dfa884a6201d2b1ce892763ff75c9706de
rossparsons1 <kataisk2002@gmail.com>
2023-08-19 18:23:14 +0500

    Update 1.3(new slider)

--- src/script.js
+++ src/script.js
@@ -1,26 +1,31 @@
+// let slideIndex = 1;
+// showSlides(slideIndex);
+//
+// function nextSlide() {
+//     showSlides(slideIndex += 1);
+// }
+//
+// function previousSlide() {
+//     showSlides(slideIndex -= 1);
+// }
+//
+// function showSlides(n) {
+//     let slides = document.getElementsByClassName("item");
+//     if (n > slides.length) {
+//         slideIndex = 1
+//     }
+//     if (n < 1) {
+//         slideIndex = slides.length
+//     }
+//     /* Проходим по каждому слайду в цикле for: */
+//     for (let slide of slides) {
+//         slide.style.display = "none";
+//     }
+//     /* Делаем элемент блочным: */
+//     slides[slideIndex - 1].style.display = "flex";
+// }
+
 let slideIndex = 1;
-showSlides(slideIndex);
+const slides = document.querySelectorAll('.item');
 
-function nextSlide() {
-    showSlides(slideIndex += 1);
-}
-
-function previousSlide() {
-    showSlides(slideIndex -= 1);
-}
-
-function showSlides(n) {
-    let slides = document.getElementsByClassName("item");
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    /* Проходим по каждому слайду в цикле for: */
-    for (let slide of slides) {
-        slide.style.display = "none";
-    }
-    /* Делаем элемент блочным: */
-    slides[slideIndex - 1].style.display = "flex";
-}
+console.log(slides);
