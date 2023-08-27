f97daf698143b1d834ce57c03ffb861ebf4b3a92
rossparsons1 <kataisk2002@gmail.com>
2023-08-28 04:17:00 +0500

    Update 1.5(add popup)

---
+++ src/script.js
@@ -0,0 +1,22 @@
+const certificates = document.querySelectorAll('.certificates_pic');
+const popup = document.querySelector('.popup_elems');
+const pic = popup.querySelector('.popup_elem');
+const close_button = popup.querySelector('.close_button');
+
+for (let i = 0; i < certificates.length; i++) {
+    certificates[i].addEventListener('click', show_certificates)
+    console.log(i);
+}
+
+close_button.addEventListener('click', close_popup);
+
+function show_certificates(){
+    if (!popup.classList.contains('active')){
+        popup.classList.add('active');
+        pic.style.backgroundImage = `url(${this.src})`;
+    }
+}
+
+function close_popup(){
+    popup.classList.remove('active');
+}
