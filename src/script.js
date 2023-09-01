63ce205d66d58e177f19a41904c8e5ef212b6f4b
rossparsons1 <kataisk2002@gmail.com>
2023-09-01 23:19:38 +0500

    Update 1.7

--- src/script.js
+++ src/script.js
@@ -1,21 +1,34 @@
+const html = document.querySelector('html')
 const certificates = document.querySelectorAll('.certificates_pic');
-const popup = document.querySelector('.popup_elems');
-const pic = popup.querySelector('.popup_elem');
-const close_button = popup.querySelector('.close_button');
+const cer_popup = document.querySelector('.cer');
+const pic = cer_popup.querySelector('.popup_elem');
+const cer_close = cer_popup.querySelector('.cer_close');
+
+const price_button = document.querySelector('.price_list');
+const price_popup = document.querySelector('.text_popup');
+const text_close = price_popup.querySelector('.text_close');
+
+price_button.addEventListener('click', open_popup)
+text_close.addEventListener('click', close_popup);
+cer_close.addEventListener('click', close_popup);
 
 for (let i = 0; i < certificates.length; i++) {
-    certificates[i].addEventListener('click', show_certificates)
+    certificates[i].addEventListener('click', show_certificates);
 }
 
-close_button.addEventListener('click', close_popup);
-
-function show_certificates(){
-    if (!popup.classList.contains('active')){
-        popup.classList.add('active');
-        pic.style.backgroundImage = `url(${this.src})`;
-    }
+function open_popup(){
+    this.parentNode.parentNode.lastElementChild.classList.add('active_popup');
+    html.style.overflowY = 'hidden';
 }
 
 function close_popup(){
-    popup.classList.remove('active');
+    this.parentNode.parentNode.lastElementChild.classList.remove('active_popup');
+    html.style.overflowY = 'scroll';
+}
+
+function show_certificates(){
+    if (!cer_popup.classList.contains('active_popup')){
+        pic.style.backgroundImage = `url(${this.src})`;
+        this.parentNode.parentNode.lastElementChild.classList.add('active_popup');
+    }
 }
